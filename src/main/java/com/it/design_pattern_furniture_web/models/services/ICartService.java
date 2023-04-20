961f816477f47be1bee4b72300facb4ecc3b50f6
MDung0807 <dominhdung21082002@gmail.com>
2023-04-20 23:28:13 +0700

    update view_models, service, repository

---
+++ src/main/java/com/it/design_pattern_furniture_web/models/services/ICartService.java
@@ -0,0 +1,27 @@
+package com.it.design_pattern_furniture_web.models.services;
+
+import com.it.design_pattern_furniture_web.models.view_models.cart_items.CartItemCreateRequest;
+import com.it.design_pattern_furniture_web.models.view_models.cart_items.CartItemGetPagingRequest;
+import com.it.design_pattern_furniture_web.models.view_models.cart_items.CartItemUpdateRequest;
+import com.it.design_pattern_furniture_web.models.view_models.cart_items.CartItemViewModel;
+
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public interface ICartService {
+    int insertCartItem(CartItemCreateRequest request);
+    boolean updateCartItem(CartItemUpdateRequest request);
+    boolean deleteCartItem(Integer cartItemId);
+    CartItemViewModel retrieveCartItemById(Integer cartItemId);
+    ArrayList<CartItemViewModel> retrieveAllCartItem(CartItemGetPagingRequest request);
+    boolean deleteCartByUserId(int userId);
+    ArrayList<CartItemViewModel> retrieveCartByUserId(int userId);
+    int getCartIdByUserId(int userId);
+    CartItemViewModel getCartItemContain(int cartId, int productId);
+    int canUpdateQuantity(int cartItemId, int quantity);
+    void updateQuantityByProductId(int productId, int quantity);
+    String addProductToCart(int productId, int quantity, int userId);
+
+    BigDecimal getTotalCartItemPriceByUserId(int userId);
+}
---
+++ src/main/java/com/it/design_pattern_furniture_web/models/view_models/cart_items/CartItemGetPagingRequest.java
@@ -0,0 +1,6 @@
+package com.it.design_pattern_furniture_web.models.view_models.cart_items;
+
+import common.paging.PagingRequest;
+
+public class CartItemGetPagingRequest extends PagingRequest {
+}
