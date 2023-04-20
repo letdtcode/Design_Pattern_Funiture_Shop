2001636f9f3c8f3a2e8ef354516ea1a6ae329ed2
tuandangvan <tuandangvann@gmail.com>
2023-04-21 01:49:28 +0700

    create products, product_images, review_items

---
+++ src/main/java/com/it/design_pattern_furniture_web/models/view_models/product_images/ProductImageGetPagingRequest.java
@@ -0,0 +1,6 @@
+package com.it.design_pattern_furniture_web.models.view_models.product_images;
+
+import common.paging.PagingRequest;
+
+public class ProductImageGetPagingRequest extends PagingRequest {
+}
---
+++ src/main/java/com/it/design_pattern_furniture_web/models/view_models/review_items/ReviewItemGetPagingRequest.java
@@ -0,0 +1,6 @@
+package com.it.design_pattern_furniture_web.models.view_models.review_items;
+
+import common.paging.PagingRequest;
+
+public class ReviewItemGetPagingRequest extends PagingRequest {
+}
