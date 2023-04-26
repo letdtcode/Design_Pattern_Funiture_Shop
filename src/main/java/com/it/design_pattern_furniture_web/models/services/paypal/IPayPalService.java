107ce2b7d7b1fe2e214a6161e07740248a7201ba
nhatminhpham772 <nhatminh772002@gmail.com>
2023-04-26 09:49:41 +0700

    api service

---
+++ src/main/java/com/it/design_pattern_furniture_web/models/services/paypal/IPayPalService.java
@@ -0,0 +1,21 @@
+package com.it.design_pattern_furniture_web.models.services.paypal;
+
+import com.it.design_pattern_furniture_web.models.view_models.cart_items.CartItemViewModel;
+import com.paypal.api.payments.Payer;
+import com.paypal.api.payments.Payment;
+import com.paypal.api.payments.RedirectUrls;
+import com.paypal.api.payments.Transaction;
+import com.paypal.base.rest.PayPalRESTException;
+
+import java.util.ArrayList;
+
+public interface IPayPalService {
+    String authorizePayment(ArrayList<CartItemViewModel> cartItems, OrderCreateRequest orderCreateRequest, String context) throws PayPalRESTException;
+    Payer getPayerInformation(OrderCreateRequest orderCreateRequest);
+    RedirectUrls getRedirectUrls(String context);
+    ArrayList<Transaction> getTransactionInformation(ArrayList<CartItemViewModel> orderItems, OrderCreateRequest orderCreateRequest);
+    String getApprovalLink(Payment approvedPayment);
+    Payment getPaymentDetails(String paymentId) throws PayPalRESTException;
+    Payment handlePayment(String paymentId, String payerId)
+            throws PayPalRESTException;
+}
